971797941daeb2c1c5d7342e6057875d98de3a8d
Dan Vo <danvo.vo@gmail.com>
2022-04-13 18:00:49 -0700

    add comments and clean up code

--- markov.js
+++ markov.js
@@ -19,12 +19,13 @@ class MarkovMachine {
 		let chains = {};
 
 		for (let i = 0; i < this.words.length; i++) {
+			// for the length of the words array, loop through each unique word and collect the words that occur after it.
 			let word = this.words[i];
 			let nextWord = this.words[i + 1] || null;
 
 			if (!chains[word]) {
 				chains[word] = Array(nextWord);
-				// Creates a new Array object if does not yet exist
+				// If the current word is not in the chain yet, it creates a new Array object if does not yet exist. New key/value pair
 			} else {
 				chains[word].push(nextWord);
 				// if it does exist, push the next word to the array
@@ -36,17 +37,18 @@ class MarkovMachine {
 	// Picks a random choice from the array
 	static randomChoice(array) {
 		return array[Math.floor(Math.random() * array.length)];
+		// picks a random word based on its index in the array
 	}
 
 	/** return random text from chains */
 
 	makeText(numWords = 100) {
-		// TODO
 		let keys = Object.keys(this.chains); // method returns an array of a given object's own enumerable property names
-		let nextWord = MarkovMachine.randomChoice(keys);
-		let output = Array(nextWord);
+		let nextWord = MarkovMachine.randomChoice(keys); // selects a random word from the array of keys
+		let output = Array(nextWord); // initializes output with an array including the random word
 
 		while (output.length < numWords && nextWord !== null) {
+			// while the length of the output array is less than numWords (100) and the next word is not null, keep selecting a random nextWord
 			let chain = this.chains[nextWord];
 			nextWord = MarkovMachine.randomChoice(chain);
 
@@ -55,6 +57,7 @@ class MarkovMachine {
 			}
 		}
 		return output.join(' ');
+		// once loop is complete, concatenate the array into a single string to return the final output
 	}
 }
 
